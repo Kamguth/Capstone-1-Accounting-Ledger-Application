13c5909d4690522ff949d95cdfc10fdc892154f7
Kamron Guthrie <kamrontguthrie@gmail.com>
2025-04-28 16:23:13 -0400

    Added bufferedreader for transaction.csv

--- AccountingLedgerApplication/src/main/java/com/pluralsight/TransactionManager.java
+++ AccountingLedgerApplication/src/main/java/com/pluralsight/TransactionManager.java
@@ -1,4 +1,26 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TransactionManager {
+    private static String fileName = "transactions.csv";
+
+    public static List<Transaction> loadTransactions() {
+        List<Transaction> transactions = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine())!= null) {
+                String[] parts = line.split("\\|");
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
